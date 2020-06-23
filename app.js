70a9319556e6f694c18a07f186b56ec2576fd732
studyboy <arifin.puk@gmail.com>
2020-06-23 14:17:58 +0800

    express testing

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World\n');
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello Yes!Man. Good day');
+})
+
+app.get('/mars', function (req, res) {
+    res.send('Hello Yes!Man from MARS. ;-)');
+})
+
+app.listen(8080, function() {
+    console.log('Welcome to Red Hat OpenShift Introduction Course.')
+})
