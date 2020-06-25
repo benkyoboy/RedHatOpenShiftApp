d989f3cbe589d13d78e344b37d54ed55b70a7bb6
unknown <arifin.puk@gmail.com>
2020-06-25 20:47:56 +0800

    version genhis khan

--- raja/app.js
+++ raja/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-    res.send('What a GREAT Day. YiiHaaa.. I am Raja!');
+    res.send('What a GREAT Day. YiiHaaa.. I am Raja! My Favourite hero is Genghis Khan.');
 })
 
 app.listen(8080, function () {
