8fc365002c87b90635380d57e400dcd9c9a7690d
unknown <arifin.puk@gmail.com>
2020-06-25 20:59:31 +0800

    version 3

--- raja/app.js
+++ raja/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-    res.send('What a GREAT Day. YiiHaaa.. I am Raja! My Favourite hero is Genghis Khan.');
+    res.send('What a GREAT Day. YiiHaaa.. I am Raja! My Favourite hero is Genghis Khan.<br/> &copy; Copyright 2020 - Arif Cloud Solutions');
 })
 
 app.listen(8080, function () {
