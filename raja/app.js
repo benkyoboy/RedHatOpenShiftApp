d82408dad7d5e642a7a5405bcca96361a94b94ae
unknown <arifin.puk@gmail.com>
2020-06-25 20:12:59 +0800

    added listerner to raja

--- raja/app.js
+++ raja/app.js
@@ -4,3 +4,7 @@ app = express();
 app.get('/', function (req, res) {
     res.send('What a GREAT Day. YiiHaaa.. I am Raja!');
 })
+
+app.listen(8080, function () {
+    console.log('Server listening on port 8080...');
+});
